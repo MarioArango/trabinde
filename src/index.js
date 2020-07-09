75994c4359808df876d93d617d566215bb55836a
Mario Arango <mlimfx@gmail.com>
2020-07-09 16:32:36 -0500

    sin morgan

--- src/index.js
+++ src/index.js
@@ -23,7 +23,7 @@ const storage = multer.diskStorage({
 
 //MIDDLEWARES
 app.use(cors())
-app.use(morgan('dev'));
+//app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(multer({
