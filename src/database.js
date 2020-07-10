c2e4aca918ce15ecae0a8eceb6ce641a7f58ae9e
Mario Arango <mlimfx@gmail.com>
2020-07-10 09:43:23 -0500

    pool

--- src/database.js
+++ src/database.js
@@ -3,21 +3,21 @@ dotenv.config();
 import mysql from 'mysql';
 //import util from 'util';
 
-const mysqlConnection = mysql.createConnection({
+const mysqlConnection = mysql.createPool({
+    connectionLimit: 10,
     host: 'us-cdbr-east-02.cleardb.com',
     user: 'b6eb6851569774',
     password: '21d8ae96',
-    database: 'heroku_677a3052d794aa4',
-    dialect: 'mysql'
+    database: 'heroku_677a3052d794aa4'
 });
 
-mysqlConnection.connect(function (err) {
+/*mysqlConnection.connect(function (err) {
     if (err) {
         console.error(`Error de conexion: ${err}`);
         return;
     }
     console.log(`Conectado a MySQL`);
-});
+});*/
 
 //const query = util.promisify(mysqlConnection.query).bind(mysqlConnection);
 
