7e028787a47061d1a209305e58df54b59f48d98b
Mario Arango <mlimfx@gmail.com>
2020-07-10 16:54:43 -0500

    asdasd

--- src/database.js
+++ src/database.js
@@ -3,7 +3,7 @@
 const mysql = require('mysql');
 //import util from 'util';
 
-const mysqlConnection = mysql.createPool({
+const mysqlConnection = mysql.mysqlConnection({
     connectionLimit: 10,
     host: 'us-cdbr-east-02.cleardb.com',
     user: 'b9d3e2959e62be',
@@ -11,13 +11,13 @@ const mysqlConnection = mysql.createPool({
     database: 'heroku_e57934221f5d2a7'
 });
 
-/*mysqlConnection.connect(function (err) {
+mysqlConnection.connect(function (err) {
     if (err) {
         console.error(`Error de conexion: ${err}`);
         return;
     }
     console.log(`Conectado a MySQL`);
-});*/
+});
 
 //const query = util.promisify(mysqlConnection.query).bind(mysqlConnection);
 
