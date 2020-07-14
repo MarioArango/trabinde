e9e96cb43e3def2061b3ff419797871138d33a14
Mario Arango <mlimfx@gmail.com>
2020-07-13 22:00:10 -0500

    asd

--- src/index.js
+++ src/index.js
@@ -21,7 +21,6 @@ const storage = multer.diskStorage({
 })
 
 //MIDDLEWARES
-app.use(cors())
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Access-Control-Allow-Request-Method');
