ff1438a6532495d72979ad78ac51a7787bbb551a
Mario Arango <mlimfx@gmail.com>
2020-07-21 19:33:22 -0500

    s

--- src/util/crearToken.js
+++ src/util/crearToken.js
@@ -8,7 +8,7 @@ token.signToken = (id) => {
     const payload = {
         id,
         iat: moment().unix(), //CUANDO FUE CREADO EL TOKEN
-        exp: moment().add(60, 'minute').unix() //CUANDO VA EXPIRAR EL TOKEN
+        exp: moment().add(1440, 'minute').unix() //CUANDO VA EXPIRAR EL TOKEN
         //MOMENT AÑADE TANTO TIEMPO AL TIEMPO UNIX QUE SE CREO ARRIBA
     }
     return jwt.sign(payload, 'a$QYgmeE$qV');
