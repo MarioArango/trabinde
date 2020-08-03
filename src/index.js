fc723eadb13421cb9f3802ce30848bb131ad1b81
Mario Arango <mlimfx@gmail.com>
2020-08-02 20:18:46 -0500

    s

--- src/index.js
+++ src/index.js
@@ -36,6 +36,14 @@ app.use((req, res, next) => {
 });
 */
 
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8100');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS,DELETE,PUT');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
+/*
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method,');
@@ -43,7 +51,7 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
   res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
   next()
-})
+})*/
 
 
 
