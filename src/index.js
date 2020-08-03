1b877d0a750e10f3411bf0b8be2c191d86ab1fe0
Mario Arango <mlimfx@gmail.com>
2020-08-02 22:38:59 -0500

    a

--- src/index.js
+++ src/index.js
@@ -45,7 +45,7 @@ app.use((req, res, next) => {
 });*/
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', 'http://localhost:8100/');
   res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method,');
   res.header('content-type: application/json; charset=utf-8')
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
